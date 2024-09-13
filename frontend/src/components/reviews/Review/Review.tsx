2e7b4a052025545a3b8d773b3e24973473834c6a
AndreiMinciuna <aminciuna@strategikonpharma.com>
2024-09-13 23:03:53 +0300

    added improvement

--- frontend/src/components/reviews/Review/Review.tsx
+++ frontend/src/components/reviews/Review/Review.tsx
@@ -1,6 +1,6 @@
 import styles from "./Review.module.css";
 
-interface PropsType {
+interface ReviewProps {
   name: string;
   src: string;
   alt: string;
@@ -8,10 +8,7 @@ interface PropsType {
   text: string;
 }
 
-const Review = (props: PropsType) => {
-  const { name, src, alt, companyName, text } = props;
-  console.log(name, src, alt, companyName, text);
-
+const Review = ({ name, src, alt, companyName, text }: ReviewProps) => {
   return (
     <div className={styles.reviewInstance}>
       <img src={src} alt={alt} className={styles.reviewImage} />
@@ -19,10 +16,7 @@ const Review = (props: PropsType) => {
       <p className={styles.companyName}>{companyName}</p>
       <hr className={styles.reviewLine} />
       <div className={styles.reviewText}>
-        <p>
-          "This is the review text from client 1. They had a great experience
-          with our service."
-        </p>
+        <p>{text}</p>
       </div>
     </div>
   );
