a3bc17dadecd84f2d2834ffee8a9617cb49e4e97
Miriam Ciurea <miriamciurea777@icloud.com>
2024-09-16 11:45:16 +0300

    cta is dynamic

--- frontend/src/components/cta/Cta.tsx
+++ frontend/src/components/cta/Cta.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './Cta.module.css';
 // import newlogo from '../../assets/newlogo.png';
 
 const Cta: React.FC = () => {
+
+  const ctaRef = useRef(null); // Reference to the div
+  const [isInView, setIsInView] = useState(false); // State to track if the section is in view
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        // If the section is intersecting (visible in the viewport), set isInView to true
+        setIsInView(entry.isIntersecting);
+      },
+      {
+        threshold: 0.5, // Adjust this threshold to when you want the class to be applied
+      }
+    );
+
+    if (ctaRef.current) {
+      observer.observe(ctaRef.current); // Start observing the ref
+    }
+
+
+    return () => {
+      if (ctaRef.current) {
+        observer.unobserve(ctaRef.current); // Clean up the observer on component unmount
+      }
+    };
+  }, []);
+
+
   return (
     <>
-      <div className={styles.cta}>
+    <div className={isInView ? `${styles.cta} ${styles.zoom} ${styles['tracking-in-expand']}` : `${styles.cta} ${styles.zoom}`} ref={ctaRef}>
         <div className={styles.title}>
           GET <span className={styles.blue}>CLOSER</span> TO YOUR <span className={styles.blue}>DREAMS</span>
         </div>
