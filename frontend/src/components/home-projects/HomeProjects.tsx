268950ee95e05a72bc8ab87881bf775762256784
Miriam Ciurea <miriamciurea777@icloud.com>
2024-09-18 18:33:51 +0300

    projects homepage fixed

--- frontend/src/components/home-projects/HomeProjects.tsx
+++ frontend/src/components/home-projects/HomeProjects.tsx
@@ -94,8 +94,6 @@ const projects = [
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat',
                 backgroundSize: 'cover',
-                transform: `translate(${offsetX * 0.1}px, ${offsetY * 0.1}px)`, // Apply horizontal and vertical parallax
-                // transition: 'transform 0.2s ease-out',
               }}
             />
             <div className={styles.projectInfo}
@@ -123,47 +121,3 @@ const projects = [
 };
 
 export default HomeProjects;
-
-
-// const projects = [
-//   {
-//     src: "https://images.unsplash.com/photo-1541888946425-d81bb19240f5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dGVhbSUyMGNvbnN0cnVjdGlvbiUyMHNpdGV8ZW58MHx8MHx8fDI%3D",
-//     alt: "Team Construction Site",
-//     info: "Kent Projects | UK"
-//   },
-//   {
-//     src: "https://images.unsplash.com/photo-1593630265256-d2cc162ab58f?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29uc3RydWN0aW9uJTIwc2l0ZXxlbnwwfHwwfHx8Mg%3D%3D",
-//     alt: "Construction Site",
-//     info: "Build Works | US"
-//   },
-//   {
-//     src: "https://images.unsplash.com/photo-1680879221403-f19bb46fe527?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHJhaWx3YXklMjB3b3JrZXJzfGVufDB8fDB8fHwy",
-//     alt: "Railway Workers",
-//     info: "Railway Project | Germany"
-//   },
-//   {
-//     src: "https://images.unsplash.com/photo-1712233462849-7db3060ba1f3?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cmFpbHdheSUyMHdvcmtlcnN8ZW58MHwxfDB8fHwy",
-//     alt: "Railway Construction",
-//     info: "Railway Works | Japan"
-//   },
-//   {
-//     src: "https://images.unsplash.com/photo-1593630987785-98139c5f3cc6?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29uc3RydWN0aW9uJTIwc2l0ZXxlbnwwfHwwfHx8Mg%3D%3D",
-//     alt: "Construction",
-//     info: "Skyline Projects | UAE"
-//   },
-//   {
-//     src: "https://images.unsplash.com/photo-1680879221403-f19bb46fe527?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHJhaWx3YXklMjB3b3JrZXJzfGVufDB8fDB8fHwy",
-//     alt: "Railway Workers",
-//     info: "Railway Project | Germany"
-//   },
-//   {
-//     src: "https://images.unsplash.com/photo-1712233462849-7db3060ba1f3?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cmFpbHdheSUyMHdvcmtlcnN8ZW58MHwxfDB8fHwy",
-//     alt: "Railway Construction",
-//     info: "Railway Works | Japan"
-//   },
-//   {
-//     src: "https://images.unsplash.com/photo-1593630987785-98139c5f3cc6?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29uc3RydWN0aW9uJTIwc2l0ZXxlbnwwfHwwfHx8Mg%3D%3D",
-//     alt: "Construction",
-//     info: "Skyline Projects | UAE"
-//   }
-// ];
