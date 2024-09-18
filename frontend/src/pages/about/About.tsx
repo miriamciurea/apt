7381a5d2efb6c31c7b7b38a5e055d7ade8946198
Serban Ciurea <ciureaserban777@icloud.com>
2024-09-18 12:28:36 +0300

    added some mirror of what was above, though it would of looked better, not responsive yet

--- frontend/src/pages/about/About.tsx
+++ frontend/src/pages/about/About.tsx
@@ -3,6 +3,7 @@ import EarlyDays from "../../components/early-days/EarlyDays";
 import AboutUsDescription from "../../components/AboutUsDescription/AboutUsDescription";
 import Team from "../../components/Team/Team";
 import AboutHeader from "../../components/about-header/aboutHeader";
+import LeftHeader from "../../components/left-header/LeftHeader";
 
 const About: React.FC = () => {
   return (
@@ -13,7 +14,8 @@ const About: React.FC = () => {
         <AboutUsDescription />
         <EarlyDays />
         <Team />
-       </div>
+        <LeftHeader />
+      </div>
     </>
   );
 };
