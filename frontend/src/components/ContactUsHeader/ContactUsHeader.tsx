b19cabd8e277e3d2db24be8c50a4649c5d37adbf
AndreiMinciuna <aminciuna@strategikonpharma.com>
2024-09-19 23:12:09 +0300

    added contact us

---
+++ frontend/src/components/ContactUsHeader/ContactUsHeader.tsx
@@ -0,0 +1,18 @@
+import styles from "./ContactUsHeader.module.css";
+
+const ContactUsHeader = () => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>WORK WITH US</h1>
+      <a href="/join-our-team" className={styles.cta}>
+        Join Our Team
+        <span className={styles.chevrons}>
+          <i className="fas fa-chevron-right"></i>
+          <i className="fas fa-chevron-right"></i>
+        </span>
+      </a>
+    </header>
+  );
+};
+
+export default ContactUsHeader;
---
+++ frontend/src/pages/ContactUs/ContactUs.tsx
@@ -0,0 +1,13 @@
+import ContactUsForm from "../../components/ContactUsForm/ContactUsForm";
+import ContactUsHeader from "../../components/ContactUsHeader/ContactUsHeader";
+
+const ContactUs = () => {
+  return (
+    <>
+      <ContactUsHeader />
+      <ContactUsForm />
+    </>
+  );
+};
+
+export default ContactUs;
