ff323a9eac27fca87a85cf849d85ea3cc7e0c1df
Miriam Ciurea <miriamciurea777@icloud.com>
2024-09-25 12:19:13 +0300

    ????  from frontend to root

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
