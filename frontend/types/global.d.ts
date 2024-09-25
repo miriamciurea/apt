cc579a40948b3737d94dbefab8a686c8d57b8e1d
Miriam Ciurea <miriamciurea777@icloud.com>
2024-09-25 11:06:11 +0300

    ????  from frontend to root

--- frontend/types/global.d.ts
+++ frontend/types/global.d.ts
@@ -1,4 +1,13 @@
+
 declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module '*.module.css';
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.mp4';
+declare module '*.lottie';
