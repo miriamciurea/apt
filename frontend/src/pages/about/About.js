762e79b92f4475e183d92b23cf6729115af96962
Serban Ciurea <ciureaserban777@icloud.com>
2024-10-29 10:36:08 +0200

    noice

--- frontend/src/pages/about/About.js
+++ frontend/src/pages/about/About.js
@@ -4,8 +4,9 @@ import AboutUsDescription from "../../components/AboutUsDescription/AboutUsDescr
 import Team from "../../components/Team/Team";
 import AboutHeader from "../../components/about-header/aboutHeader";
 import LeftHeader from "../../components/left-header/LeftHeader";
+import Reviews from "../../components/reviews/Reviews";
 import Cta from "../../components/cta/Cta";
 const About = () => {
-    return (_jsx(_Fragment, { children: _jsxs("div", { className: "container", children: [_jsx(AboutHeader, {}), _jsx(AboutUsDescription, {}), _jsx(EarlyDays, {}), _jsx(Team, {}), _jsx(LeftHeader, {}), _jsx(Cta, {})] }) }));
+    return (_jsx(_Fragment, { children: _jsxs("div", { className: "container", children: [_jsx(AboutHeader, {}), _jsx(AboutUsDescription, {}), _jsx(EarlyDays, {}), _jsx(Team, {}), _jsx(LeftHeader, {}), _jsx(Reviews, {}), _jsx(Cta, {})] }) }));
 };
 export default About;
