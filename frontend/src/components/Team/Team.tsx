56bbf698b32889d921c633655719e4dc378cfc06
Serban Ciurea <ciureaserban777@icloud.com>
2024-11-07 13:24:23 +0200

    changed the titles in about us

--- frontend/src/components/Team/Team.tsx
+++ frontend/src/components/Team/Team.tsx
@@ -4,7 +4,7 @@ const teamMembers = [
   {
     name: "Adrian Pearson",
     alt: "Adrian Pearson",
-    role: "Operations Director",
+    role: "Director",
     img: "https://plus.unsplash.com/premium_photo-1683121771856-3c3964975777?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
